a813ff6bb8a9a5692570b21bdc7be06555ff3b40
Anne Fadia Ikhfa <annefadia143@gmail.com>
2023-02-26 21:54:43 +0700

    Shop DemoQA Commit

--- src/test/java/com/juaracoding/selenium/TestTMinggu3.java
+++ src/test/java/com/juaracoding/selenium/TestTMinggu3.java
@@ -1,6 +1,8 @@
 package com.juaracoding.selenium;
 
 import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
@@ -14,7 +16,7 @@ import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.By.className;
 
-public class TestMThree {
+public class TestTMinggu3 {
     WebDriver driver;
 
     @BeforeClass
@@ -45,8 +47,25 @@ public class TestMThree {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,400)");
         String txtName = driver.findElement(By.xpath("//input[@id='username']")).getText();
-        String txt
         Assert.assertTrue(txtName.contains("AnneFadia143"));
+        String txtPassword = driver.findElement(By.xpath("//@id='password']")).getText();
+        Assert.assertTrue(txtPassword.contains("1403Pane");
+    }
+
+    @Test
+    public void tesAddCart(){
+        driver.findElement(By.xpath("//div[3]//div[1]//div[1]//div[2]//div[1]//div[1]//div[1]//a[1]//img[1]")).click();
+        System.out.println("sow item");
+        WebElement selectColor =  driver.findElement(By.id("pa_color"));
+        Select color = new Select(selectColor);
+        driver.findElement(By.id("pa_size"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,400)");
+        driver.findElement(By.xpath("//*[@id=\"product-1281\"]/div[1]/div[2]/form/div/div[2]/div/div/button[2]/i")).click();
+        String AddToCart = driver.findElement(By.xpath("//button[normalize-space()='Add to cart']")).getText();
+        System.out.println(AddToCart);
+        String Heel = driver.findElement(By.xpath("//*[@id=\"noo-site\"]/div[2]/div[2]/div/div/div[3]/div/span[1]")).getText();
+        Assert.assertTrue(Heel.contains("shoes"));
     }
 
     @AfterClass
